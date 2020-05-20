a16f7855abaff51f1afdb9f293f8804fff961b05
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2020-05-19 20:10:42 -0400

    project cleanup and updating functions/models

--- eda_functions.py
+++ eda_functions.py
@@ -3,7 +3,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
+from IPython.display import HTML
+from io import BytesIO
+from base64 import b64encode
 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 
 def common_plot_setup(figure=None, axes=None, palette=None, font=None, figsize=(15, 10), style=None):
@@ -40,6 +44,9 @@ def plot_keras_fit_history(results, epoch_offset=0, common_plot_kwargs={}):
     plt.show()
     return history
 
+def map_char_codes(labels, classmap):
+    df = pd.DataFrame(pd.Series(labels, name="index")).copy()
+    return df.merge(classmap, how="left", on="index")["char"]
 
 def plot_keras_test_evaluation(listed_scores, index_offset=0, epoch_per_test=1, common_plot_kwargs={}):
     fig, ax = common_plot_setup(**common_plot_kwargs)
@@ -48,3 +55,50 @@ def plot_keras_test_evaluation(listed_scores, index_offset=0, epoch_per_test=1, 
     sns.lineplot(data=history, ax=ax)
     plt.show()
     return history
+
+def pil_to_html_img_tag(image):
+    b = BytesIO()
+    image.save(b, format='png')
+    return f"<img src='data:image/png;base64,{b64encode(b.getvalue()).decode('utf-8')}'/>"
+
+
+def bulk_character_viewer(data, labels, indices=(1), predictions=None, columns=3):
+    """Allows viewing of multiple images and their labels/predictions in a single cell divided into columns."""
+    shape = data.shape
+
+    # Sets aside a simple boolean to prevent index errors if predictions are omitted
+    skip_pred = type(predictions) != pd.core.series.Series
+
+    # Checks and adjusts to make sure the color channel is included.
+    if shape != 4:
+        data = data.reshape([shape[0], shape[1], shape[2], 1]).copy()
+
+    # Loops through the range adding html lines for a raw image and it's labels.
+    code_lines = []
+    for i in np.arange(*indices):
+        img, label = pil_to_html_img_tag(array_to_img(data[i])), f"<p>{labels[i]}</p>"
+        if skip_pred:
+            line = f"<span>{img} {label}<br></span>"
+        else:
+            label = f"<p>Actual: {labels[i]}</p>"
+            prediction = f"<p>Predicted: {predictions[i]}</p>"
+            line = f"<span>{img} {label} {prediction}<br></span>"
+        code_lines.append(line)
+
+    # Puts the list of lines into a block of HTML
+    code = "\n".join(code_lines)
+
+    # Returns html seperated by the amount of columns.
+    return HTML(f"<div style='column-count: {columns};'>{code}</div>")
+
+
+def colors_from_values(values, palette_name):
+    """Creates a palette that changes coloring of bar plots to vary by their y value"""
+
+    # normalize the values to range [0, 1]
+    normalized = (values - min(values)) / (max(values) - min(values))
+    # convert to indices
+    indices = np.round(normalized * (len(values) - 1)).astype(np.int32)
+    # use the indices to get the colors
+    palette = sns.color_palette(palette_name, len(values))
+    return np.array(palette).take(indices, axis=0)
--- modeling_functions.py
+++ modeling_functions.py
@@ -1,4 +1,6 @@
+import numpy as np
 from imblearn.over_sampling import SMOTE
+from sklearn.metrics import balanced_accuracy_score, balanced_accuracy_score
 
 from random_lumberjacks.src.random_lumberjacks.model.model_classes import *
 
@@ -9,6 +11,10 @@ def color_values_to_float(array, bits):
     bit_dict = {8:255, 10:1023, 12:4095}
     return array/bit_dict[bits]
 
+def determine_trainable_layers(model):
+    for layer in model.layers:
+        print(layer.name, layer.trainable)
+    print(f"{len(model.trainable_weights)} trainable weights")
 
 def images_to_1d(data, w=None, h=None, channels=None, inverse=False):
     """Function that will turn conform pixel data to their vector representation in order to be used in Neural Networks.
@@ -30,6 +36,8 @@ def images_to_1d(data, w=None, h=None, channels=None, inverse=False):
 
     return data.reshape(args)
 
+def pad_raster_edges(data, x_pad=0, y_pad=0):
+    return np.pad(data, [(0,0),(x_pad,x_pad),(y_pad,y_pad)], constant_values=(0))
 
 def preprocess_raster_resampling(X, y, resample, random_state=None):
     if resample == "upsample":
@@ -46,3 +54,22 @@ def preprocess_raster_resampling(X, y, resample, random_state=None):
         print("Ignoring class imbalances")
     return X, y
 
+def generate_keras_model(model, layers=[], compile_kwargs={}):
+    model = model
+    for layer in layers:
+        model.add(layer)
+    model.compile(**compile_kwargs)
+    model.summary()
+    return model
+
+def image_class_accuracy_scores(y_act, y_hat):
+    acc = accuracy_score(y_act, y_hat)
+    bal_acc = balanced_accuracy_score(y_act, y_hat)
+    print(f"Accuracy: {acc}")
+    print(f"Balanced Accuracy: {bal_acc}")
+    return acc, bal_acc
+
+def image_class_evaluation(model, X, y):
+    y_act = np.argmax(y, axis = 1)
+    y_hat = np.argmax(model.predict(X), axis=1)
+    return image_class_accuracy_scores(y_act, y_hat)
