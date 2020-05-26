c661a792c7c34e33fefce93082bc2da23c3f4957
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2020-05-26 14:54:35 -0400

    app input to exported img workflow

--- modeling_functions.py
+++ modeling_functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics import balanced_accuracy_score, balanced_accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
 
 
 def determine_trainable_layers(model):
